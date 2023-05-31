5a912f026c376674672a52d00864e8c657e31949
oldfang42 <tedfung0402@gmail.com>
2023-05-31 10:52:21 +0800

    hw4

---
+++ examples/threading/threading.c
@@ -0,0 +1,78 @@
+#include "threading.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Optional: use these functions to add debug or error prints to your application 
+#define DEBUG_LOG(msg,...)
+//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
+void* threadfunc(void* thread_param)
+{
+
+    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    // hint: use a cast like the one below to obtain thread arguments from your parameter
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    thread_func_args->thread_complete_success=false;
+    *(thread_func_args->thread)=pthread_self();
+
+    usleep((thread_func_args->wait_to_obtain_ms)*1000);
+    int retLock=pthread_mutex_lock(thread_func_args->mutex);
+
+    if(retLock==0){
+            usleep((thread_func_args->wait_to_release_ms)*1000);
+            int retUnLock=pthread_mutex_unlock(thread_func_args->mutex);
+            if(retUnLock==0){
+                    thread_func_args->thread_complete_success=true;
+                    DEBUG_LOG("mutex locked and released successfully\n");
+            }
+            else{
+                    ERROR_LOG("pthread_mutex_unlock failed with return=%d\n", retUnLock);
+            }
+    }
+    else{
+        ERROR_LOG("pthread_mutex_lock failed with return=%d\n", retLock);
+    }
+
+    return thread_param;
+}
+
+
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+{
+    /**
+     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * using threadfunc() as entry point.
+     *
+     * return true if successful.
+     *
+     * See implementation details in threading.h file comment block
+     */
+
+    struct thread_data* thread_param = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    if(thread_param!=NULL){
+
+            thread_param->thread = thread;
+            thread_param->mutex = mutex;
+            thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+            thread_param->wait_to_release_ms = wait_to_release_ms;
+
+            pthread_t t;
+            int ret = pthread_create( &t, NULL, threadfunc, thread_param);
+            if(ret==0){
+                DEBUG_LOG("Thread created successfully\n");
+                return true;
+            }
+            else{
+                ERROR_LOG("pthread_create failed with return=%d\n", ret);
+            }
+
+    }
+
+    DEBUG_LOG("malloc failed\n");
+
+    return false;
+}
---
+++ finder-app/dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y u-boot-tools
+sudo apt-get install -y qemu
